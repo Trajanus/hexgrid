96a32024b5d52777e3b84bdadca4a8acbb8878a1
Trajanus <derektgoodwin@gmail.com>
2017-09-05 00:56:50 -0700

    Initial commit.
    First unit test passed.

---
+++ hexgrid-unit-test/HexUnitTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using hexgrid;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace hexgrid_unit_test
+{
+    [TestClass]
+    public class HexUnitTest
+    {
+        private Hex testHex = new Hex();
+        public HexUnitTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void GetCoordinatesUnitTest()
+        {
+
+        }
+    }
+}
